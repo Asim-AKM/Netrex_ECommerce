4419897418d65ee3683239e2dab8fbcd664a37f9
Affan Wajid <karori1121@gmail.com>
2025-12-06 20:16:55 +0500

    feature: ProductModuleDocumentationOnly

--- Natrex_Ecommerce/Domain_Service/Entities/ProductManagmentModule/ProductCategory.cs
+++ Natrex_Ecommerce/Domain_Service/Entities/ProductManagmentModule/ProductCategory.cs
@@ -2,10 +2,23 @@
 
 namespace Domain_Service.Entities.ProductAndCategoryModule
 {
+    /// <summary>
+    /// Represents a product category used to classify products
+    /// within the system.
+    /// </summary>
     public class ProductCategory
     {
+        /// <summary>
+        /// Gets or sets the unique identifier of the product category.
+        /// This value serves as the primary key.
+        /// </summary>
         [Key]
         public Guid CategoryId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the display name of the product category.
+        /// This name is used for categorizing and filtering products.
+        /// </summary>
         public string CategoryName { get; set; } = string.Empty;
     }
 }
