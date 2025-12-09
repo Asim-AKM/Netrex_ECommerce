a1859564a8fd743800766ba23e9fc5116a9c28ba
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-09 21:48:31 +0500

    feat: Implement Seller Payout module with create, fetch, and update functionality

---
+++ Natrex_Ecommerce/APIGateway_Service/Controllers/SellerPayoutController.cs
@@ -0,0 +1,58 @@
+﻿using Application_Service.DTO_s.Payment_PayoutDtos;
+using Application_Service.Services.PaymentAndPayoutServices.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIGateway_Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellerPayoutController : ControllerBase
+    {
+        private readonly ISellerPayoutManager _sellerPayoutService;
+        public SellerPayoutController(ISellerPayoutManager sellerPayoutService)
+        {
+            _sellerPayoutService = sellerPayoutService;
+        }
+
+        [HttpPost("CreateSellerPayout")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateSellerPayout([FromBody] AddSellerPayoutDto request)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await _sellerPayoutService.CreateSellerPayout(request);
+            return Created("", "Seller Payout Created Successfully");
+        }
+
+        [HttpGet("GetSellerPayoutById/{sellerPayoutId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetSellerPayoutById([FromRoute] Guid sellerPayoutId)
+        {
+            var payout = await _sellerPayoutService.GetSellerPayoutById(sellerPayoutId);
+            if (payout == null)
+            {
+                return NotFound();
+            }
+            return Ok(payout);
+        }
+
+        [HttpPut("UpdateSellerPayoutAsPaid/{sellerPayoutId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> UpdateSellerPayoutAsPaid([FromRoute] Guid sellerPayoutId)
+        {
+            var payout = await _sellerPayoutService.GetSellerPayoutById(sellerPayoutId);
+            if (payout == null)
+            {
+                return NotFound();
+            }
+            await _sellerPayoutService.UpdateSellerPayoutAsPaid(sellerPayoutId);
+            return Ok("Seller Payout Updated As Paid Successfully");
+        }
+    }
+}
---
+++ Natrex_Ecommerce/Application_Service/Services/PaymentAndPayoutServices/Interface/ISellerPayoutManager.cs
@@ -0,0 +1,17 @@
+﻿using Application_Service.DTO_s.Payment_PayoutDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Services.PaymentAndPayoutServices.Interface
+{
+    public interface ISellerPayoutManager
+    {
+        Task CreateSellerPayout(AddSellerPayoutDto dto);
+        Task UpdateSellerPayoutAsPaid(Guid sellerPayoutId);
+        Task<FetchSellerPayoutDto> GetSellerPayoutById(Guid sellerPayoutId);
+
+    }
+}
