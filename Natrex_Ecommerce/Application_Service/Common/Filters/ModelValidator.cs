b6a3c13dfeca2ee7b04181004fbd703f49e0736f
Shoaib96978 <muhammadshoaob26@gmail.com>
2025-12-11 16:57:34 +0500

    feat : FluentValidation for login and Registration Dto's

---
+++ Natrex_Ecommerce/Application_Service/Common/Filters/ModelValidator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Application_Service.Common.Filters
+{
+    internal class ModelValidator : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+          // pending 
+        }
+    }
+}
---
+++ Natrex_Ecommerce/Application_Service/DTO's/Validators/UserMangement/LoginDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using Application_Service.DTO_s.UserManagmentDto_s;
+using FluentValidation;
+
+namespace Application_Service.DTO_s.Validators.UserMangement
+{
+    internal class LoginDtoValidator:AbstractValidator<LoginDto>
+    {
+        public LoginDtoValidator() 
+        {
+            RuleFor(x => x.UserIdentifier)
+                .NotEmpty().WithMessage("UserIdentifier Required")
+                .EmailAddress().WithMessage("Invalid Email");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password required")
+                .NotNull().WithMessage("Password should not null");
+        }
+    }
+}
