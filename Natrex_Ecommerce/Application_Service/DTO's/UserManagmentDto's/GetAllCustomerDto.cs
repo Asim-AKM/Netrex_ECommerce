81e5c688a9e5af188baa3b52c92d715be2da095e
Muhammad Asim <164513926+Asim-AKM@users.noreply.github.com>
2025-12-11 18:18:04 +0500

    Update GetAllCustomerDto.cs

--- Natrex_Ecommerce/Application_Service/DTO's/UserManagmentDto's/GetAllCustomerDto.cs
+++ Natrex_Ecommerce/Application_Service/DTO's/UserManagmentDto's/GetAllCustomerDto.cs
@@ -6,6 +6,6 @@ using System.Threading.Tasks;
 
 namespace Application_Service.DTO_s.UserManagmentDto_s
 {
-    public record GetAllCustomerDto(Guid UserId,Guid CustomerId, string City, string Province, string Country, string Address);
+    public record GetAllCustomerDto(Guid CustomerId,Guid UserId, string City, string Province, string Country, string Address);
    
 }
