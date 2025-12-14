5d8e47bd45bf8bd637a796937a370da19e1de961
Muhammad Ahmad <muhammadahmad0346913@gmail.com>
2025-12-14 20:01:30 +0500

    User Table GetAll-Delete-implementation

--- Natrex_Ecommerce/Domain_Service/RepoInterfaces/UserManagment/IUserRoleRepo.cs
+++ Natrex_Ecommerce/Domain_Service/RepoInterfaces/UserManagment/IUserRoleRepo.cs
@@ -5,5 +5,6 @@ namespace Domain_Service.RepoInterfaces.UserManagment
     public  interface IUserRoleRepo
     {
         Task AddUserRole(UserRole role);
+        Task<UserRole> GetRolebyFK(Guid userId);
     }
 }
