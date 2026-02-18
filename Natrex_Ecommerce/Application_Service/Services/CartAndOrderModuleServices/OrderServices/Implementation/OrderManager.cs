f3276161cc1f2b22eead06e2c5d670b845a8b1a8
MuhammadShehzad709 <muhammadshehzaddeveloper@gmail.com>
2026-02-18 13:35:56 +0500

    bug fix in products

--- Natrex_Ecommerce/Application_Service/Services/CartAndOrderModuleServices/OrderServices/Implementation/OrderManager.cs
+++ Natrex_Ecommerce/Application_Service/Services/CartAndOrderModuleServices/OrderServices/Implementation/OrderManager.cs
@@ -3,6 +3,7 @@ using Application_Service.Common.Mappers.CartAndOrderModuleMappers.OrderMappers;
 using Application_Service.DTO_s.CartAndOrderDtos.OrderDtos;
 using Application_Service.Services.CartAndOrderModuleServices.OrderServices.Interface;
 using Domain_Service.Entities.CartAndOrderModule;
+using Domain_Service.Entities.ProductAndCategoryModule;
 using Domain_Service.Enums;
 using Domain_Service.RepoInterfaces.CartAndOrderRepo.CartRepos;
 using Domain_Service.RepoInterfaces.CartAndOrderRepo.OrderRepos;
@@ -26,33 +27,37 @@ namespace Application_Service.Services.CartAndOrderModuleServices.OrderServices.
                return ApiResponse<GetOrderDto>.Fail("Cart is Empty",ResponseType.NotFound);
             }
             var order = orderDto.Map();
-            await _unitOfWork.Orders.Create(order);
-            double totalAmount = 0;
-            foreach(var items in cartItems)
+            var productList = new List<(Product product, int quantity)>();
+            foreach (var items in cartItems)
             {
-                var product =await _unitOfWork.Products.GetById(items.ProductId);
-                if(product == null)
+                var product = await _unitOfWork.Products.GetById(items.ProductId);
+                if (product == null)
                     return ApiResponse<GetOrderDto>.Fail("Product not found", ResponseType.NotFound);
                 if (product.StockQuantity < items.Quantity)
                     return ApiResponse<GetOrderDto>.Fail($"Product {product.ProductName} is out of stock", ResponseType.BadRequest);
-
+                order.TotalAmount += items.Quantity * product.Price;
+                productList.Add((product, items.Quantity));
+                await _unitOfWork.CartItems.Delete(items.CartItemId);
+            }   
+            await _unitOfWork.Orders.Create(order);      
+            foreach(var items in productList)
+            {
                 var orderItem = new OrderItem
                     {
                         OrderItemId = Guid.NewGuid(),
                         OrderId = order.OrderId,
-                        ProductId = items.ProductId,
-                        Quantity = items.Quantity,
-                        Price = product.Price,
-                        PriceTotal = product.Price * items.Quantity
+                        ProductId = items.product.ProductId,
+                        Quantity = items.quantity,
+                        Price = items.product.Price,
+                        PriceTotal = items.product.Price * items.quantity
                     };
-                totalAmount += orderItem.PriceTotal;
+             
                 await _unitOfWork.OrderItems.Create(orderItem);
             }
-            order.TotalAmount = totalAmount;
-            await _unitOfWork.Orders.Update(order);
-            foreach (var item in cartItems)
-                await _unitOfWork.CartItems.Delete(item.CartItemId);
-            await _unitOfWork.SaveChangesAsync();  
+            if(await _unitOfWork.SaveChangesAsync() <= 0)
+            {
+                return ApiResponse<GetOrderDto>.Fail("Failed to create order", ResponseType.InternalServerError);
+            }  
             return ApiResponse<GetOrderDto>.Success(order.Map(),"Order created successfully", ResponseType.Ok);
         }
         public async Task<ApiResponse<bool>> CancelOrderAsync(Guid orderId)
