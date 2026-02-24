91e9d045f1554048d0770244f5e3d6896aa592a8
Shoaib96978 <muhammadshoaob26@gmail.com>
2026-02-24 21:46:34 +0500

    feature : Add Another EndPoint which return only jwt after succefuly lohin

---
+++ Natrex_Ecommerce/Application_Service/DTO's/UserManagmentDto's/UserSessionDto's/LoginResultDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application_Service.DTO_s.UserManagmentDto_s.UserSessionDto_s
+{
+    public class LoginResultDto
+    {
+        public string JwtToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+    }
+}
--- Natrex_Ecommerce/Application_Service/Services/UserManagmentServices/Interface/IAuthenticationManager.cs
+++ Natrex_Ecommerce/Application_Service/Services/UserManagmentServices/Interface/IAuthenticationManager.cs
@@ -1,5 +1,6 @@
 ﻿using Application_Service.Common.APIResponses;
 using Application_Service.DTO_s.UserManagmentDto_s;
+using Application_Service.DTO_s.UserManagmentDto_s.UserSessionDto_s;
 using Application_Service.DTO_s.UsersDto.Accounts;
 
 namespace Application_Service.Services.UserManagmentServices.Interface
@@ -8,9 +9,10 @@ namespace Application_Service.Services.UserManagmentServices.Interface
     {
         Task<ApiResponse<CreateUserDto>> CreateUserAsync(CreateUserDto request);
         Task<ApiResponse<string>> VerifyRegistrationOtpAsync(VerifyRegistrationOtpDto request);
-        Task<ApiResponse<object>> ResendRegistrationOtpAsync(string email);
-        Task<ApiResponse<object>> LoginAsync(LoginDto request);
-        Task<ApiResponse<object>> ForgetPasswordAsync(string userIdentifier);
+        Task<ApiResponse<string>> ResendRegistrationOtpAsync(string email);
+        Task<ApiResponse<LoginResultDto>> LoginAsync(LoginDto request);
+        Task<ApiResponse<string>> SignInAsync(LoginDto request);
+        Task<ApiResponse<string>> ForgetPasswordAsync(string userIdentifier);
         Task<ApiResponse<string>> ConfirmOtp(CheckOtpDto request);
         Task<ApiResponse<object>> LogoutAsync(string refreshToken);
     }
