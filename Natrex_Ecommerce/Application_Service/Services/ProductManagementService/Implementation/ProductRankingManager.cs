02ab9c1f3301c0e421d45d66815e9de4399fddf3
Muhammad Ahmad <muhammadahmad0346913@gmail.com>
2026-02-27 21:01:42 +0500

    Get Images

--- Natrex_Ecommerce/Application_Service/Services/ProductManagementService/Implementation/ProductRankingManager.cs
+++ Natrex_Ecommerce/Application_Service/Services/ProductManagementService/Implementation/ProductRankingManager.cs
@@ -1,9 +1,70 @@
-﻿using Application_Service.Services.ProductManagementService.Interfaces;
+﻿using Application_Service.Common.APIResponses;
+using Application_Service.Common.Mappers.ProductMapper;
+using Application_Service.DTO_s.ProductDTOS;
+using Application_Service.Services.ProductManagementService.Interfaces;
+using Domain_Service.Entities.ProductAndCategoryModule;
+using Domain_Service.Enums;
+using Domain_Service.RepoInterfaces.ProductRepo;
 
 namespace Application_Service.Services.ProductManagementService.Implementation
 {
     public class ProductRankingManager : IProductRankingManager
     {
-        
+        private readonly IProductRankingRepo _productRankingRepo;
+        private readonly IProductImageRepo _productImageRepo;
+        public ProductRankingManager(IProductRankingRepo productRankingRepo, IProductImageRepo productImageRepo)
+        {
+            _productRankingRepo = productRankingRepo;
+            _productImageRepo = productImageRepo;
+        }
+        public async Task<ApiResponse<List<GetProductDto>>> GetBestSellersAsync()
+        {
+            var product= await _productRankingRepo.GetBestSellersAsync();
+            if (product == null || !product.Any())
+                return ApiResponse<List<GetProductDto>>.Fail("No products found", ResponseType.NotFound);
+            var images = await _productImageRepo.GetAllProductImages();
+            var dto = product.GetAllProducts(images);
+            return ApiResponse<List<GetProductDto>>.Success(dto, "Best sellers retrieved successfully");
+        }
+
+        public async Task<ApiResponse<List<GetProductDto>>> GetHomepageProductsAsync()
+        {
+            var product= await _productRankingRepo.GetHomepageProductsAsync();
+                if (product == null || !product.Any())
+                    return ApiResponse<List<GetProductDto>>.Fail("No products found", ResponseType.NotFound);
+                var images = await _productImageRepo.GetAllProductImages();
+                var dto = product.GetAllProducts(images);
+            return ApiResponse<List<GetProductDto>>.Success(dto, "Homepage products retrieved successfully");
+        }
+
+        public async Task<ApiResponse<List<GetProductDto>>> GetNewArrivalsAsync()
+        {
+            var product= await _productRankingRepo.GetNewArrivalsAsync();
+            if (product == null || !product.Any())
+                return ApiResponse<List<GetProductDto>>.Fail("No products found", ResponseType.NotFound);
+            var images = await _productImageRepo.GetAllProductImages();
+            var dto = product.GetAllProducts(images);
+            return ApiResponse<List<GetProductDto>>.Success(dto, "New arrivals retrieved successfully");
+        }
+
+        public async Task<ApiResponse<List<GetProductDto>>> GetTopRatedAsync()
+        {
+            var product= await _productRankingRepo.GetTopRatedAsync();
+            if (product == null || !product.Any())
+                return ApiResponse<List<GetProductDto>>.Fail("No products found", ResponseType.NotFound);
+            var images = await _productImageRepo.GetAllProductImages();
+            var dto = product.GetAllProducts(images);
+            return ApiResponse<List<GetProductDto>>.Success(dto, "Top rated products retrieved successfully");
+        }
+
+        public async Task<ApiResponse<List<GetProductDto>>> GetTrendingAsync()
+        {
+            var product= await _productRankingRepo.GetTrendingAsync();
+            if (product == null || !product.Any())
+                return ApiResponse<List<GetProductDto>>.Fail("No products found", ResponseType.NotFound);   
+            var images = await _productImageRepo.GetAllProductImages();
+            var dto = product.GetAllProducts(images);
+            return ApiResponse<List<GetProductDto>>.Success(dto, "Trending products retrieved successfully");
+        }
     }
 }
--- Natrex_Ecommerce/Application_Service/Services/ProductManagementService/Interfaces/IProductRankingManager.cs
+++ Natrex_Ecommerce/Application_Service/Services/ProductManagementService/Interfaces/IProductRankingManager.cs
@@ -1,7 +1,14 @@
-﻿namespace Application_Service.Services.ProductManagementService.Interfaces
+﻿using Application_Service.Common.APIResponses;
+using Application_Service.DTO_s.ProductDTOS;
+
+namespace Application_Service.Services.ProductManagementService.Interfaces
 {
     public interface IProductRankingManager
     {
-      
+        Task<ApiResponse<List<GetProductDto>>> GetBestSellersAsync();
+        Task<ApiResponse<List<GetProductDto>>> GetTrendingAsync();
+        Task<ApiResponse<List<GetProductDto>>> GetTopRatedAsync();
+        Task<ApiResponse<List<GetProductDto>>> GetHomepageProductsAsync();
+        Task<ApiResponse<List<GetProductDto>>> GetNewArrivalsAsync();
     }
 }
