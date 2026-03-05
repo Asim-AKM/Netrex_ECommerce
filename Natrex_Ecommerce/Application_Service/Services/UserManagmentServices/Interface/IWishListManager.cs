3c3f1186e36ed7664a05d9e2c5fd8282d1c27f7e
Muhammad Fayaz <mfayaz21703@gmail.com>
2026-03-06 00:07:51 +0500

    Change AddWishListItem return type to string
    Updated the IWishListManager interface so that AddWishListItem now returns Task<ApiResponse<string>> instead of Task<ApiResponse<Guid>>. This alters the method's response from a GUID to a string upon successful addition of a wishlist item. No other interface methods were changed.

--- Natrex_Ecommerce/Application_Service/Services/UserManagmentServices/Interface/IWishListManager.cs
+++ Natrex_Ecommerce/Application_Service/Services/UserManagmentServices/Interface/IWishListManager.cs
@@ -2,7 +2,7 @@
 {
     public interface IWishListManager
     {
-        Task<ApiResponse<Guid>> AddWishListItem(AddWishListItemDto request);
+        Task<ApiResponse<string>> AddWishListItem(AddWishListItemDto request);
         Task<ApiResponse<string>> DeleteWishListItem(Guid wishListItemid);
         Task<ApiResponse<List<GetWishListItem>>> GetWishListItems(Guid userId);
         Task<ApiResponse<int>> GetWishListCount(Guid userId);
